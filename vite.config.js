c5136d3cafc18d3487c991a3a3ba2451a37cdfab
Omicron <Omigod@live.ru>
2024-12-20 22:37:03 +0300

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
       find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
