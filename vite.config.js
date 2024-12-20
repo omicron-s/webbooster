2fc4a49ecffce03b45f2d0465314d3a01746a215
Omicron <Omigod@live.ru>
2024-12-20 22:45:19 +0300

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "/webbooster/src"),
       // '@': fileURLToPath(new URL('./src', import.meta.url)),
       // find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))
     }
